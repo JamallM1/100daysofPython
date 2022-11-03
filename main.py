a2e80c765bc52c23a8c2c482918b3957a47b8e1f
Jamall Malik <mimsjamall79@gmail.com>
2022-11-03 13:22:58 -0400

    concatenate 100days of coding

---
+++ main.py
@@ -0,0 +1,10 @@
+# Concatenate
+yourName = input("What is your name: ")
+yourYear = input("What year is it: ")
+print(yourName, yourYear, "Thank you for that information")
+
+yourFood = input("Name a food: ")
+yourPlant = input("Name a type of plant: ")
+cookingStyle = input("Your cooking style: ")
+itemDiy = input("Name a DIY item: ")
+print(yourFood, "with", yourPlant, "and a", itemDiy)
